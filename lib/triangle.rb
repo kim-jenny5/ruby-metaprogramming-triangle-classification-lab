20a34c8fff6f02f9bb835c2a270787a99981b772
Jenny Kim <kimj1905@gmail.com>
2021-03-26 11:15:49 -0400

    commit tree e6d002978aa7dba95336fd49908f1462b837956a

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,10 @@ class Triangle
   end
 
   def check?
-    # if [side1, side2, side3].each {|side| side <= 0} 
+    # if [side1, side2, side3].find {|side| side <= 0}
+
+    # end
+    # binding.pry
     if side1 <= 0 || side2 <= 0 || side3 <= 0|| (side1 + side2) <= side3 || (side2 + side3) <= side1 || (side1 + side3) <= side2
       true
     else
